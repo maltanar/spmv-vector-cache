53086b464adb567f3d5463a97ddb9452631658fe
Yaman Umuroglu <maltanar@gmail.com>
2014-10-24 00:02:51 +0900

    shell script for Chisel->Verilog->SystemC

---
+++ simple-dm-cache/verilate-systemc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+SRC_DIR="/home/maltanar/sandbox/spmv-vector-cache/simple-dm-cache"
+WORK_DIR="/home/maltanar/sandbox/build-verilator"
+TARGET_DIR="/home/maltanar/sandbox/spmv-cache-systemc"
+
+
+mkdir -p $WORK_DIR
+mkdir -p $TARGET_DIR
+cd $SRC_DIR
+sbt "run --targetDir $WORK_DIR --backend v"
+cd $WORK_DIR
+verilator --sc SimpleDMVectorCache.v +define+SYNTHESIS+1 -Wno-lint
+cp obj_dir/*.cpp $TARGET_DIR/
+cp obj_dir/*.h $TARGET_DIR/
+cd $SRC_DIR
+rm -rf $WORK_DIR
