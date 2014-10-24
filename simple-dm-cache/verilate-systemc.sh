3edd105c5e1ea794f85537b943957ab160ea7574
Yaman Umuroglu <maltanar@gmail.com>
2014-10-24 14:38:42 +0200

    update verilator script to use cp -f and add verilator.cpp

--- simple-dm-cache/verilate-systemc.sh
+++ simple-dm-cache/verilate-systemc.sh
@@ -2,7 +2,8 @@
 
 SRC_DIR="/home/maltanar/sandbox/spmv-vector-cache/simple-dm-cache"
 WORK_DIR="/home/maltanar/sandbox/build-verilator"
-TARGET_DIR="/home/maltanar/sandbox/spmv-cache-systemc"
+TARGET_DIR="/home/maltanar/sandbox/spmvaccsim/vector-cache-src"
+VERILATOR_DIR="/usr/share/verilator/include"
 
 export SYSTEMC_INCLUDE="/home/maltanar/systemc/include"
 export SYSTEMC_LIBDIR="/home/maltanar/systemc/lib-linux64"
@@ -13,7 +14,9 @@ cd $SRC_DIR
 sbt "run --targetDir $WORK_DIR --backend v"
 cd $WORK_DIR
 verilator --sc SimpleDMVectorCache.v +define+SYNTHESIS+1 -Wno-lint
-cp obj_dir/*.cpp $TARGET_DIR/
-cp obj_dir/*.h $TARGET_DIR/
+cp -f obj_dir/*.cpp $TARGET_DIR/
+cp -f obj_dir/*.h $TARGET_DIR/
+cp -f $VERILATOR_DIR/verilated.cpp $TARGET_DIR
+
 cd $SRC_DIR
 rm -rf $WORK_DIR
