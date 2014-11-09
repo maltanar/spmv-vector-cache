a66a6c54153ed2977baf44efd236cdfa3efecf81
Yaman Umuroglu <maltanar@gmail.com>
2014-11-09 20:18:45 +0100

    cache generation script for baseline

--- simple-dm-cache/Main.scala
+++ simple-dm-cache/Main.scala
@@ -3,9 +3,18 @@ import Chisel._
 
 object MainObj {
   def main(args: Array[String]): Unit = {
+  def getCacheDepthParam(list: List[String]) : Int = {
+      list match {
+        case "--depth" :: value :: tail => value.toInt
+        case _ => 1024
+      }
+    }
+    
     val cacheLineSize = 64
-    val cacheDepth = 1024*128
-    val addressBits = 24    
+    val cacheDepth = getCacheDepthParam(args.toList)
+    val addressBits = 24
+    
+    println(cacheDepth)
     //chiselMain(args, () => Module(new CacheDataMemory(cacheLineSize, cacheDepth, addressBits)))
     chiselMainTest(args, () => Module(new SimpleDMVectorCache(cacheLineSize, cacheDepth, addressBits))) { c => new SimpleDMVectorCacheTester(c, cacheDepth) }
   }
