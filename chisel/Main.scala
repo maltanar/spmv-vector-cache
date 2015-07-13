a30c5d8e8118815f663b6851613254b7bb7bc1ab
Yaman Umuroglu <maltanar@gmail.com>
2015-07-13 23:47:40 +0200

    restructure Main for separate cmdline-based verilog/test generators

--- chisel/Main.scala
+++ chisel/Main.scala
@@ -1,6 +1,8 @@
 package SpMVAccel
 
 import Chisel._
+import TidbitsAXI._
+import TidbitsSimUtils._
 
 object MainObj {
   val testOutputDir = "testOutput/"
@@ -14,20 +16,53 @@ object MainObj {
     return Array( "--targetDir", verilogOutputDir+cmpName, "--v")
   }
 
+  val p = new SpMVAccelWrapperParams()
+
+  def printUsageAndExit() {
+    println("Usage: sbt \"run <op> <comp>\"")
+    println("where <op> = {inst | test}")
+    System.exit(0)
+  }
+
+  val instFxnMap: Map[String, () => Module] = Map(
+    "SpMVAccel" -> {() => new SpMVAccelerator(p)}
+  )
+
+  val testFxnMap: Map[String, () => AXIWrappableAccel] = Map {
+    "TestSpMVBackend" -> {() => new TestSpMVBackend()}
+  }
+
   def main(args: Array[String]): Unit = {
-    val cmpName = args(0)
-    println("Executing task: " + cmpName)
+    if(args.size != 2) { printUsageAndExit() }
 
-    val instFxnMap: Map[String, () => Module] = Map(
-      "SpMVAccel" -> {() => new SpMVAccelerator(new SpMVAccelWrapperParams()) }
-    )
+    val op = args(0)
+    val harnessMemDepth = 8*1024*1024
 
-    val instFxn = instFxnMap(cmpName)
-    makeVerilog(cmpName, instFxn)
+    val cmpName = args(1)
+    if(op == "inst") {
+      val instFxn = instFxnMap(cmpName)
+      makeVerilog(cmpName, instFxn)
+    } else if(op == "test") {
+      val testFxn = testFxnMap(cmpName)
+      makeHarnessTest(cmpName, harnessMemDepth, testFxn)
+    } else {
+      printUsageAndExit()
+    }
   }
 
   def makeVerilog(cmpName: String, instFxn: () => Module) {
     val vargs = makeVerilogBuildArgs(cmpName)
     chiselMain(vargs, () => Module(instFxn()))
+  }
+
+  def makeHarnessTest(cmpName: String,
+                      memDepth: Int,
+                      fxn: () => AXIWrappableAccel) {
+    val targs = makeTestArgs(cmpName)
+    val instModule = {() => Module(new WrappableAccelHarness(fxn, memDepth))}
+    val instTest = {c => new WrappableAccelTester(c)}
+
+    val aT = makeTestArgs(cmpName)
+    chiselMainTest(aT, instModule)(instTest)
   }
 }
