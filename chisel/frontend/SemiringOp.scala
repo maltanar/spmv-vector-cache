2e45992b01095811bd973a3c96e54e1e9762d3f8
Yaman Umuroglu <maltanar@gmail.com>
2015-07-20 01:22:46 +0200

    add staged UInt op generator based on systolic registers

--- chisel/frontend/SemiringOp.scala
+++ chisel/frontend/SemiringOp.scala
@@ -43,7 +43,53 @@ class OpMulCombinatorial(w: Int) extends SemiringOp(w) {
   io.in.ready := io.out.ready
 }
 
-// TODO add systolic reg to parametrize op stages flexibly
+// systolic reg to parametrize op stages flexibly
+// this essentially behaves like a single-element queue with no
+// fallthrough, so it can be used to add forced latency to an op
+// the ready signal is still combinatorially linked to allow fast
+// handshakes, like a Chisel queue with pipe=true flow=false
+class SystolicReg(w: Int) extends Module {
+  val io = new Bundle {
+    val in = Decoupled(UInt(width = w)).flip
+    val out = Decoupled(UInt(width = w))
+  }
+  val regValid = Reg(init = Bool(false))
+  val regData = Reg(init = UInt(0, w))
+  val allowNewData = (!regValid || io.out.ready)
+
+  io.out.bits := regData
+  io.out.valid := regValid
+  io.in.ready := allowNewData
+
+  when(allowNewData) {
+    regData := io.in.bits
+    regValid := io.in.valid
+  }
+}
+
+// generate operator (defined by fxn) with n-cycle latency
+// mostly intended to simulate high-latency ops, won't make much sense
+// in synthesis since all "useful work" is carried out before entering
+// the delay pipe anyway
+class StagedUIntOp(w: Int, n: Int, fxn: (UInt, UInt) => UInt)
+extends SemiringOp(w) {
+  override lazy val latency: Int = n
+  if(latency == 0) {
+    println("StagedUIntOp needs at least 1 stage")
+    System.exit(-1)
+  }
+  // connect transformed input to first stage
+  val delayPipe = Vec.fill(n) {Module(new SystolicReg(w)).io}
+  delayPipe(0).in.valid := io.in.valid
+  delayPipe(0).in.bits := fxn(io.in.bits.first, io.in.bits.second)
+  io.in.ready := delayPipe(0).in.ready
+  // connect stages
+  for(i <- 0 until n-1) {
+    delayPipe(i+1).in <> delayPipe(i).out
+  }
+  // connect last stage to output
+  io.out <> delayPipe(n-1).out
+}
 
 // 1-stage variants of UInt add and multiply
 class OpAddSingleStage(w: Int) extends SemiringOp(w) {
