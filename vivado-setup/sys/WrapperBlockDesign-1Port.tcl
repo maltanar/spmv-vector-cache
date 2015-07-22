101b275481b9bde38f8528d84e2e695cdc58eb6b
Yaman Umuroglu <maltanar@gmail.com>
2015-07-22 16:36:00 +0200

    do comp synthesis in repo dirs +multiport wrapper + update .gitignore

--- vivado-setup/sys/WrapperBlockDesign-1Port.tcl
+++ vivado-setup/sys/WrapperBlockDesign-1Port.tcl
@@ -174,25 +174,25 @@ proc create_root_design { parentCell } {
   set rst_processing_system7_0_100M [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 rst_processing_system7_0_100M ]
 
   # Create interface connections
-  connect_bd_intf_net -intf_net AXIAccelWrapper_0_mem [get_bd_intf_pins AXIAccelWrapper_0/mem] [get_bd_intf_pins axi_mem_intercon/S00_AXI]
+  connect_bd_intf_net -intf_net AXIAccelWrapper_0_mem0 [get_bd_intf_pins AXIAccelWrapper_0/mem0] [get_bd_intf_pins axi_mem_intercon/S00_AXI]
   connect_bd_intf_net -intf_net axi_mem_intercon_M00_AXI [get_bd_intf_pins axi_mem_intercon/M00_AXI] [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
   connect_bd_intf_net -intf_net processing_system7_0_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins processing_system7_0/DDR]
   connect_bd_intf_net -intf_net processing_system7_0_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins processing_system7_0/FIXED_IO]
   connect_bd_intf_net -intf_net processing_system7_0_M_AXI_GP0 [get_bd_intf_pins processing_system7_0/M_AXI_GP0] [get_bd_intf_pins processing_system7_0_axi_periph/S00_AXI]
-  connect_bd_intf_net -intf_net processing_system7_0_axi_periph_M00_AXI [get_bd_intf_pins AXIAccelWrapper_0/csr] [get_bd_intf_pins processing_system7_0_axi_periph/M00_AXI]
-  connect_bd_intf_net -intf_net processing_system7_0_axi_periph_M01_AXI [get_bd_intf_pins AXIOutputRegisters_0/S00_AXI] [get_bd_intf_pins processing_system7_0_axi_periph/M01_AXI]
+  connect_bd_intf_net -intf_net processing_system7_0_axi_periph_M00_AXI [get_bd_intf_pins AXIOutputRegisters_0/S00_AXI] [get_bd_intf_pins processing_system7_0_axi_periph/M00_AXI]
+  connect_bd_intf_net -intf_net processing_system7_0_axi_periph_M01_AXI [get_bd_intf_pins AXIAccelWrapper_0/csr] [get_bd_intf_pins processing_system7_0_axi_periph/M01_AXI]
 
   # Create port connections
   connect_bd_net -net AXIOutputRegisters_0_reg0_output [get_bd_pins AXIOutputRegisters_0/reg0_output] [get_bd_pins reset_slice/Din]
   connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins AXIAccelWrapper_0/clk] [get_bd_pins AXIOutputRegisters_0/s00_axi_aclk] [get_bd_pins axi_mem_intercon/ACLK] [get_bd_pins axi_mem_intercon/M00_ACLK] [get_bd_pins axi_mem_intercon/S00_ACLK] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK] [get_bd_pins processing_system7_0_axi_periph/ACLK] [get_bd_pins processing_system7_0_axi_periph/M00_ACLK] [get_bd_pins processing_system7_0_axi_periph/M01_ACLK] [get_bd_pins processing_system7_0_axi_periph/S00_ACLK] [get_bd_pins rst_processing_system7_0_100M/slowest_sync_clk]
   connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_processing_system7_0_100M/ext_reset_in]
+  connect_bd_net -net reset_slice_Dout [get_bd_pins AXIAccelWrapper_0/reset] [get_bd_pins reset_slice/Dout]
   connect_bd_net -net rst_processing_system7_0_100M_interconnect_aresetn [get_bd_pins axi_mem_intercon/ARESETN] [get_bd_pins processing_system7_0_axi_periph/ARESETN] [get_bd_pins rst_processing_system7_0_100M/interconnect_aresetn]
   connect_bd_net -net rst_processing_system7_0_100M_peripheral_aresetn [get_bd_pins AXIOutputRegisters_0/s00_axi_aresetn] [get_bd_pins axi_mem_intercon/M00_ARESETN] [get_bd_pins axi_mem_intercon/S00_ARESETN] [get_bd_pins processing_system7_0_axi_periph/M00_ARESETN] [get_bd_pins processing_system7_0_axi_periph/M01_ARESETN] [get_bd_pins processing_system7_0_axi_periph/S00_ARESETN] [get_bd_pins rst_processing_system7_0_100M/peripheral_aresetn]
-  connect_bd_net -net xlslice_0_Dout [get_bd_pins AXIAccelWrapper_0/reset] [get_bd_pins reset_slice/Dout]
 
   # Create address segments
-  create_bd_addr_seg -range 0x20000000 -offset 0x0 [get_bd_addr_spaces AXIAccelWrapper_0/mem] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_processing_system7_0_HP0_DDR_LOWOCM
-  create_bd_addr_seg -range 0x10000 -offset 0x40000000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs AXIAccelWrapper_0/csr/reg0] SEG_AXIAccelWrapper_0_reg0
+  create_bd_addr_seg -range 0x20000000 -offset 0x0 [get_bd_addr_spaces AXIAccelWrapper_0/mem0] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_processing_system7_0_HP0_DDR_LOWOCM
+  create_bd_addr_seg -range 0x1000 -offset 0x40000000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs AXIAccelWrapper_0/csr/reg0] SEG_AXIAccelWrapper_0_reg0
   create_bd_addr_seg -range 0x1000 -offset 0x43C00000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs AXIOutputRegisters_0/S00_AXI/S00_AXI_reg] SEG_AXIOutputRegisters_0_S00_AXI_reg
   
 
--- vivado-setup/sys/sys-sources.xdc
+++ vivado-setup/sys/sys-sources.xdc
@@ -2,4 +2,4 @@ source /home/maltanar/Dropbox/PhD/ip-repo/ZynqWrappers/BRAMNetlist.xdc
 source /home/maltanar/Dropbox/PhD/ip-repo/ZynqWrappers/QueueNetlist.xdc
 source /home/maltanar/Dropbox/PhD/ip-repo/ZynqWrappers/DPNetlist.xdc
 
-read_edif /home/maltanar/fpga-sandbox/SpMVAccel-BufferAll-Comp/AXIAccelWrapper.edif
+read_edif /home/maltanar/sandbox/spmv-vector-cache/vivado-setup/comp/AXIAccelWrapper.edif
