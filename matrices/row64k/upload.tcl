7e0cc24c21092bab16942e0f53ea2c888de98ee7
Yaman Umuroglu <maltanar@gmail.com>
2015-07-24 14:32:51 +0200

    add 64k row vector matrix for hazard testing

---
+++ matrices/row64k/upload.tcl
@@ -0,0 +1,6 @@
+connect arm hw
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/row64k/row64k-meta.bin 0x08000100
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/row64k/row64k-indptr.bin 0x08000140
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/row64k/row64k-inds.bin 0x08040180
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/row64k/row64k-data.bin 0x08080180
+disconnect 64
