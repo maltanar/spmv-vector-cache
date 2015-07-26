9b478918a3cb4eb80f7001a7399b79af9fdfb80d
Yaman Umuroglu <maltanar@gmail.com>
2015-07-26 13:29:32 +0200

    example data: add 64k identity matrix

---
+++ matrices/i64k/upload.tcl
@@ -0,0 +1,6 @@
+connect arm hw
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64k/i64k-meta.bin 0x08000100
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64k/i64k-indptr.bin 0x08000140
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64k/i64k-inds.bin 0x08040180
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64k/i64k-data.bin 0x08080180
+disconnect 64
