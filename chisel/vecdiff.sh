b8c87e977b0cf9ad59e6ed522954af1ca4248e2a
Yaman Umuroglu <maltanar@gmail.com>
2015-07-29 15:14:46 +0200

    reverse endianness in vecdiff to make viewing easier

--- chisel/vecdiff.sh
+++ chisel/vecdiff.sh
@@ -3,7 +3,11 @@
 GOLDEN="../matrices/$1/golden.bin"
 TEST="out-$1.bin"
 
-xxd -g8 -c8 -ps $GOLDEN > A.txt
-xxd -g8 -c8 -ps $TEST > B.txt
+perl -e 'open F,shift; do { read(F,$a,8); print scalar reverse($a);} while(!eof(F));' $GOLDEN > golden-rev.bin
+perl -e 'open F,shift; do { read(F,$a,8); print scalar reverse($a);} while(!eof(F));' $TEST > test-rev.bin
+
+
+xxd -g8 -c8 -ps golden-rev.bin > A.txt
+xxd -g8 -c8 -ps test-rev.bin > B.txt
 
 diff A.txt B.txt
