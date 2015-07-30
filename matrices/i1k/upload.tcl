7da0e8732e2bf91eb1b8de2c0dd014d638786aa9
Yaman Umuroglu <maltanar@gmail.com>
2015-07-31 01:38:45 +0200

    add a few more small example matrices

---
+++ matrices/i1k/upload.tcl
@@ -0,0 +1,6 @@
+connect arm hw
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i1k/i1k-meta.bin 0x08000100
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i1k/i1k-indptr.bin 0x08000140
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i1k/i1k-inds.bin 0x08001180
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i1k/i1k-data.bin 0x08002180
+disconnect 64
---
+++ matrices/i64/upload.tcl
@@ -0,0 +1,6 @@
+connect arm hw
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64/i64-meta.bin 0x08000100
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64/i64-indptr.bin 0x08000140
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64/i64-inds.bin 0x08000280
+dow -data /home/maltanar/sandbox/spmv-vector-cache/matrices/i64/i64-data.bin 0x08000380
+disconnect 64
