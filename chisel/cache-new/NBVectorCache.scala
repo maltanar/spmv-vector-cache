74ca0f74457d3ba5d3c57c539ec64e3c879c90d5
Yaman Umuroglu <maltanar@gmail.com>
2015-08-10 23:29:45 +0200

    tasklist to implement nonblocking changes

---
+++ chisel/cache-new/NBVectorCache.scala
@@ -0,0 +1,300 @@
+package SpMVAccel
+
+import Chisel._
+import TidbitsOCM._
+import TidbitsDMA._
+
+
+// TODO changes necessary for nonblocking cache (single pending miss):
+// - pop off and register pending miss upon entering handler
+// - add "out of order" bit to response (emitted with responses under pending miss)
+// - keep hit-based tagQ <> respQ connection during miss handler (except last state)
+// - push out (in-order) response for the pending miss in the last state
+
+
+class NBVectorCache(val p: SpMVAccelWrapperParams) extends Module {
+  val io = new SinglePortCacheIF(p)
+  val pOCM = new OCMParameters( p.ocmDepth*p.opWidth, p.opWidth, p.opWidth, 2,
+                                p.ocmReadLatency)
+
+  // useful shorthands/definitions
+  val lineSize = p.opWidth
+  val addrBits = if (p.enableCMS) p.ptrWidth-1 else p.ptrWidth
+  val depth = p.ocmDepth
+  val indBitCount = log2Up(depth)
+  val tagBitCount = addrBits - indBitCount
+  def isStartOfRow(x: UInt): Bool = {x(p.ptrWidth-1)}
+  def cacheTag(x: UInt): UInt = {x(indBitCount+tagBitCount-1, indBitCount)}
+  def cacheInd(x: UInt): UInt = {x(indBitCount-1, 0)}
+  def cacheAddr(tag: UInt , ind: UInt): UInt = {Cat(tag, ind)}
+
+  // needs to be tagBits + 1 - wide (for the line valid bit)
+  val tagMem = Module(new WMTagRAM(tagBitCount+1, p.ocmDepth)).io
+  val dataMem = Module(if (p.ocmPrebuilt) new OnChipMemory(pOCM, p.ocmName) else
+                  new AsymDualPortRAM(pOCM)).io
+
+  val ddr = io.mem
+  val tagPortR = tagMem
+  val dataPortR = dataMem.ports(0)
+  val dataPortW = dataMem.ports(1)
+
+  // registers for keeping statistics
+  val regReadMissCount = Reg(init = UInt(0, 32))
+
+  // in-flight DDR write tracking logic
+  def txn[T <: Data](intf: DecoupledIO[T]): Bool = {intf.ready & intf.valid}
+  val regPendingWrites = Reg(init = UInt(0, 4))
+  // always ready to accept write responses
+  ddr.memWrRsp.ready := Bool(true)
+  val wReqTxn = txn(ddr.memWrReq)
+  val wRspTxn = txn(ddr.memWrRsp)
+  val noPendingWrites = (regPendingWrites === UInt(0))
+  // keep track of # DDR writes in flight
+  when(wReqTxn & !wRspTxn) {regPendingWrites := regPendingWrites + UInt(1)}
+  .elsewhen(!wReqTxn & wRspTxn) {regPendingWrites := regPendingWrites - UInt(1)}
+
+  /////////////////////////// cache read port ///////////////////////////
+  // cache read request shorthands
+  val rdReqValid = io.read.req.valid
+  val rdReqTag = cacheTag(io.read.req.bits)
+  val rdReqInd = cacheInd(io.read.req.bits)
+  val rdReqRowStart = isStartOfRow(io.read.req.bits)
+
+  val queueHasRoom = Bool()
+
+  // registered version of the cache read request
+  // TODO these should be ShiftRegs if tag read has higher latency
+  val regRdReqValid = Reg(next = rdReqValid & queueHasRoom)
+  val regRdReqTag = Reg(next = rdReqTag)
+  val regRdReqInd = Reg(next = rdReqInd)
+  val regRdReqRowStart = Reg(next = rdReqRowStart)
+
+  // tag + data access for read port
+  tagPortR.req.addr := rdReqInd
+  dataPortR.req.addr := rdReqInd
+
+  // shorthands for the returned read data
+  val rdCacheTag = tagPortR.rsp.readData(tagBitCount, 1)
+  // lowest bit of tag storage is used as valid indicator
+  val rdCacheValid = tagPortR.rsp.readData(0)
+  val rdCacheData = dataPortR.rsp.readData
+
+  // tag response queue
+  // TODO 2 is dependent on tag+data read latency (1) plus outstanding misses (1)?
+  val tagRespType = new FullTagResponse(indBitCount, tagBitCount, lineSize)
+  val qs: Int = 4
+  val tagRespQ = Module(new Queue(tagRespType, qs)).io
+  val qfree = UInt(qs)-tagRespQ.count
+  queueHasRoom := Bool(false)
+  tagRespQ.enq.valid := regRdReqValid
+  io.read.req.ready := queueHasRoom
+  tagRespQ.enq.bits.ind := regRdReqInd
+  tagRespQ.enq.bits.reqCMS := regRdReqRowStart
+  tagRespQ.enq.bits.reqTag := regRdReqTag
+  tagRespQ.enq.bits.rspTag := rdCacheTag
+  tagRespQ.enq.bits.rspValid := rdCacheValid
+  tagRespQ.enq.bits.rspData := rdCacheData
+  tagRespQ.deq.ready := Bool(false)
+
+  val headValid = tagRespQ.deq.valid
+  val head = tagRespQ.deq.bits
+
+  val readHit = headValid & head.rspValid & (head.reqTag === head.rspTag)
+  val readMiss = headValid & !(head.rspValid & (head.reqTag === head.rspTag))
+
+  // cache read response
+  io.read.rsp.valid := Bool(false)
+  io.read.rsp.bits := head.rspData
+
+  // read miss replacement
+  val regReadMissData = Reg(init = UInt(0, lineSize))
+  tagPortR.req.writeData := Cat(head.reqTag, Bits("b1"))
+  dataPortR.req.writeData := regReadMissData
+  // write enables will be set by state machine
+  tagPortR.req.writeEn := Bool(false)
+  dataPortR.req.writeEn := Bool(false)
+
+  /////////////////////////// cache write port ///////////////////////////
+  // cache write request shorthands
+  val wrReqValid = io.write.req.valid
+  val wrReqTag = cacheTag(io.write.req.bits.id)
+  val wrReqInd = cacheInd(io.write.req.bits.id)
+  val wrReqData = io.write.req.bits.data
+
+  // data access for write port
+  dataPortW.req.addr := wrReqInd
+  dataPortW.req.writeEn := wrReqValid
+  dataPortW.req.writeData := wrReqData
+
+  // cache write port
+  io.write.req.ready := Bool(true)  // always ready to accept writes
+  // write completion, 1 cycle after write request was valid
+  io.writeComplete := Reg(next = wrReqValid)
+
+  // default outputs
+  io.done := Bool(false)
+  // statistics
+  io.readCount := Counter32Bit(txn(io.read.req))
+  io.writeCount := Counter32Bit(txn(io.write.req))
+  io.conflictMissCount := Counter32Bit(txn(ddr.memWrReq))
+  io.readMissCount := regReadMissCount
+  io.writeMissCount := UInt(0)  // this variant can have no write misses
+
+  // DDR ports
+  val opBytes = UInt(p.opWidth/8)
+  val vecBase = io.base
+  val missLocation = cacheAddr(head.reqTag, head.ind)
+  val evictLocation = cacheAddr(head.rspTag, head.ind)
+  val evictData = head.rspData
+  // DDR read requests
+  ddr.memRdReq.valid := Bool(false)
+  ddr.memRdReq.bits.driveDefaults()
+  ddr.memRdReq.bits.numBytes := opBytes
+  ddr.memRdRsp.ready := Bool(false)
+  // the only DDR read request is to the missed index
+  ddr.memRdReq.bits.addr := vecBase + missLocation*opBytes
+  // DDR write requests
+  ddr.memWrReq.valid := Bool(false)
+  ddr.memWrReq.bits.driveDefaults()
+  ddr.memWrReq.bits.numBytes := opBytes
+  ddr.memWrReq.bits.isWrite := Bool(true)
+  // the only DDR write request is for the evicted location
+  ddr.memWrReq.bits.addr := vecBase + evictLocation*opBytes
+
+  // DDR write data
+  ddr.memWrDat.valid := Bool(false)
+  // the only DDR write data is the evicted data
+  ddr.memWrDat.bits := evictData
+
+  // cache control state machine
+  val sActive :: sFill :: sFlush :: sDone :: sReadMiss1 :: sReadMiss2 :: sReadMiss3 :: sColdMiss :: Nil = Enum(UInt(), 8)
+  val regState = Reg(init = UInt(sActive))
+  io.cacheState := regState
+  // register for fill/flush line counting
+  val regCacheInd = Reg(init = UInt(0, 32))
+
+  // use the mem wr req and data channels in lockstep
+  val canDoExtWrite = ddr.memWrReq.ready & ddr.memWrDat.ready
+  val canDoExtRead = ddr.memRdReq.ready
+
+  switch(regState) {
+    is(sActive) {
+      regCacheInd := UInt(0)
+      when (io.startWrite) {
+        regState := sFlush
+        // prefetch tag&data for line 0
+        tagPortR.req.addr := UInt(0)
+        dataPortR.req.addr := UInt(0)
+      }
+      .elsewhen (io.startInit) {regState := sFill}
+      .elsewhen(readMiss) {
+        if(p.enableCMS) {
+          regState := Mux(head.reqCMS, sColdMiss, sReadMiss1)
+        } else {regState := sReadMiss1}
+      } .otherwise {
+        queueHasRoom := (qfree > UInt(1))
+        // regular cache activity
+        tagRespQ.deq.ready := readHit & io.read.rsp.ready
+        io.read.rsp.valid := readHit
+      }
+    }
+
+    is(sFill) {
+      // initialize cache by writing invalid to all tag bits
+      when(regCacheInd === UInt(p.ocmDepth)) {regState := sDone}
+      .otherwise {
+        tagPortR.req.writeEn := Bool(true)
+        tagPortR.req.addr := regCacheInd
+        tagPortR.req.writeData := UInt(0)
+        regCacheInd := regCacheInd + UInt(1)
+      }
+    }
+
+    is(sFlush) {
+      // flush cache
+      tagPortR.req.addr := regCacheInd
+      dataPortR.req.addr := regCacheInd
+
+      val allLinesScanned = (regCacheInd === UInt(p.ocmDepth))
+
+      when(allLinesScanned) {
+        // wait til all pending writes are finished before sDone
+        when(noPendingWrites) {regState := sDone}
+      } .otherwise {
+        when(canDoExtWrite) {
+          val fetchedIndex = regCacheInd(indBitCount-1, 0)
+          val flushLocation = cacheAddr(rdCacheTag, fetchedIndex)
+          ddr.memWrDat.valid := rdCacheValid
+          ddr.memWrDat.bits := rdCacheData
+          ddr.memWrReq.valid := rdCacheValid
+          ddr.memWrReq.bits.addr := vecBase + flushLocation*opBytes
+
+          regCacheInd := regCacheInd + UInt(1)
+          tagPortR.req.addr := regCacheInd + UInt(1)
+          dataPortR.req.addr := regCacheInd + UInt(1)
+        }
+      }
+    }
+
+    is(sDone) {
+      io.done := Bool(true)
+      when(!io.startWrite & !io.startInit) {regState := sActive}
+    }
+
+    is(sColdMiss) {
+      val needEvict = head.rspValid
+      when(canDoExtWrite | needEvict) {
+        // write request only emitted on conflict miss (different valid tag)
+        ddr.memWrReq.valid := needEvict
+        ddr.memWrDat.valid := needEvict
+        regReadMissCount := regReadMissCount + UInt(1)
+        // special for cold miss handling: write 0 as cache data
+        regReadMissData := UInt(0)
+        regState := sReadMiss3
+      }
+    }
+
+    is(sReadMiss1) {
+      // wait until all pending writes are complete before proceeding with
+      // read miss handling, plus place on the read/write queues
+      val needEvict = head.rspValid
+      when(noPendingWrites & canDoExtRead & (canDoExtWrite | !needEvict)) {
+        ddr.memRdReq.valid := Bool(true) // load the missing index
+        // write request only emitted on conflict miss (different valid tag)
+        ddr.memWrReq.valid := needEvict
+        ddr.memWrDat.valid := needEvict
+        // count miss
+        regReadMissCount := regReadMissCount + UInt(1)
+        regState := sReadMiss2
+      }
+    }
+
+    is(sReadMiss2) {
+      // wait for DDR read response and save it to regReadMissData
+      ddr.memRdRsp.ready := Bool(true)
+      when(ddr.memRdRsp.valid) {
+        regReadMissData := ddr.memRdRsp.bits.readData
+        regState := sReadMiss3
+      }
+    }
+
+    is(sReadMiss3) {
+      // set addresses for tag and data write
+      tagPortR.req.addr := head.ind
+      dataPortR.req.addr := head.ind
+      // make read response available
+      io.read.rsp.valid := Bool(true)
+      io.read.rsp.bits := regReadMissData
+
+      when(io.read.rsp.ready) {
+        // pop off pending miss from head
+        // since we are in a read miss, we know it's valid for sure, no check
+        tagRespQ.deq.ready := Bool(true)
+        // update cache data and tag
+        tagPortR.req.writeEn := Bool(true)
+        dataPortR.req.writeEn := Bool(true)
+        regState := sActive
+      }
+    }
+  }
+}
---
+++ chisel/frontend/SpMVFrontendNBCache.scala
@@ -0,0 +1,172 @@
+package SpMVAccel
+
+import Chisel._
+import TidbitsAXI._
+import TidbitsStreams._
+import TidbitsOCM._
+import TidbitsDMA._
+
+// frontend using a nonblocking vector cache
+// TODO:
+// - add OoO mark bit after read response
+// - put OoO-marked responses in different queue
+// - pops from appropriate queue upon shadow queue deq. match
+
+class SpMVFrontendNBCache(val p: SpMVAccelWrapperParams) extends Module {
+  val opType = UInt(width=p.opWidth)
+  val idType = UInt(width=p.ptrWidth)
+  val opWidthIdType = new OperandWithID(p.opWidth, p.ptrWidth)
+  val opsAndId = new OperandWithID(2*p.opWidth, p.ptrWidth)
+  val shadowType = new OperandWithID(1, p.ptrWidth)
+  val syncOpType = new SemiringOperands(p.opWidth)
+
+  val io = new Bundle {
+    // control
+    val startInit = Bool(INPUT)
+    val startRegular = Bool(INPUT)
+    val startWrite = Bool(INPUT)
+
+    // status
+    val doneInit = Bool(OUTPUT)
+    val doneRegular = Bool(OUTPUT)
+    val doneWrite = Bool(OUTPUT)
+
+    // TODO debug+profiling outputs
+    val readMissCount = UInt(OUTPUT, 32)
+    val writeMissCount = UInt(OUTPUT, 32)
+    val conflictMissCount = UInt(OUTPUT, 32)
+    val hazardStalls = UInt(OUTPUT, 32)
+    val cacheState = UInt(OUTPUT, 32)
+
+    // value inputs
+    val numNZ = UInt(INPUT, width = 32)
+    val numRows = UInt(INPUT, width = 32)
+    val baseOutputVec = UInt(INPUT, width = 32)
+    // data exchange with backend
+    val colPtrIn = Decoupled(UInt(width = p.ptrWidth)).flip
+    val rowIndIn = Decoupled(UInt(width = p.ptrWidth)).flip
+    val nzDataIn = Decoupled(UInt(width = p.opWidth)).flip
+    val inputVecIn = Decoupled(UInt(width = p.opWidth)).flip
+
+    // port for res.vec reads/writes
+    val mp = new GenericMemoryMasterPort(p.toMRP())
+  }
+
+  // useful functions for working with stream forks and joins
+  val forkAll = {x: OperandWithID => x}
+  val forkShadow = {x: OperandWithID => OperandWithID(UInt(0, width=1), x.id)}
+  val forkId = {x: OperandWithID => x.id}
+  val forkOp = {x: OperandWithID => x.data}
+  val readFilter = {x: GenericMemoryResponse => x.readData}
+  def forkOps(x: OperandWithID) : SemiringOperands = {
+    val opA = x.data(2*p.opWidth-1, p.opWidth)
+    val opB = x.data(p.opWidth-1, 0)
+    return SemiringOperands(p.opWidth, opA, opB)
+  }
+  val joinOpIdOp= {
+    (a: OperandWithID, b: UInt) => OperandWithID(Cat(a.data, b), a.id)
+  }
+  val joinOpId = {(op: UInt, id: UInt) => OperandWithID(op, id)}
+  val joinOpOp = {(a: UInt, b: UInt) => SemiringOperands(p.opWidth, a, b)}
+
+  // instantiate multiply operator
+  val mul = Module(p.makeMul())
+  // instantiate StreamDelta and StreamRepeatElem
+  val deltaGenM = Module(new StreamDelta(p.ptrWidth))
+  val deltaGen = deltaGenM.io
+  deltaGen.samples <> io.colPtrIn
+  val rptGenM = Module(new StreamRepeatElem(p.opWidth, p.ptrWidth))
+  val rptGen = rptGenM.io
+  rptGen.inElem <> io.inputVecIn
+  rptGen.inRepCnt <> deltaGen.deltas
+
+  // wire up multiplier inputs with StreamJoin
+  val mulOpJoin = Module(new StreamJoin(opType, opType, syncOpType, joinOpOp)).io
+  mulOpJoin.inA <> rptGen.out
+  mulOpJoin.inB <> io.nzDataIn
+  mul.io.in <> mulOpJoin.out
+  // uncomment to debug multiplier inputs
+  /*
+  when(mulOpJoin.inA.valid & mulOpJoin.inB.valid) {
+    printf("MUL %x * %x\n", mulOpJoin.inA.bits, mulOpJoin.inB.bits)
+  }
+  */
+
+  // add a queue to buffer multiplier results, in case of stalls
+  // this will let the pipes continue a while longer
+  // TODO parametrize queue size
+  val productQ = Module(new Queue(UInt(width=p.opWidth), 16)).io
+  productQ.enq <> mul.io.out
+  // join products and rowind data to pass to reducer
+  val redJoin = Module(new StreamJoin(opType, idType, opWidthIdType, joinOpId)).io
+  redJoin.inA <> productQ.deq
+  redJoin.inB <> io.rowIndIn
+  // TODO make own module
+  val cacheM = Module(new NBVectorCache(p))
+  val cache = cacheM.io
+  cache.mem.memRdReq <> io.mp.memRdReq
+  cache.mem.memRdRsp <> io.mp.memRdRsp
+  cache.mem.memWrReq <> io.mp.memWrReq
+  cache.mem.memWrRsp <> io.mp.memWrRsp
+  // add write data queue to avoid deadlocks (cache wants req&data ready
+  // at the same time)
+  io.mp.memWrDat <> Queue(cache.mem.memWrDat, 16)
+  cache.base := io.baseOutputVec
+  cache.startInit := io.startInit
+  cache.startWrite := io.startWrite
+  io.doneInit := cache.done & io.startInit
+  io.doneWrite := cache.done & io.startWrite
+  io.readMissCount := cache.readMissCount
+  io.writeMissCount := cache.writeMissCount
+  io.conflictMissCount := cache.conflictMissCount
+  io.cacheState := cache.cacheState
+
+  // CATCH: shadow queue is narrow for NoWMVectorCache, essentially using the
+  // cacheline index bits of the element index as the comparison key
+  // this prevents operands that map to the same cacheline from entering
+  val shadow = Module(new UniqueQueue(1, log2Up(p.ocmDepth), p.issueWindow)).io
+  val cacheEntry = Module(new StreamFork(opWidthIdType, opWidthIdType, shadowType,
+                      forkAll, forkShadow)).io
+  cacheEntry.in <> redJoin.out
+  cacheEntry.outB <> shadow.enq
+
+  val cacheReadFork = Module(new StreamFork(opWidthIdType, opWidthIdType, idType, forkAll, forkId)).io
+  cacheEntry.outA <> cacheReadFork.in
+
+  val cacheWaitReadQ = Module(new Queue(opWidthIdType, p.issueWindow+1)).io
+  cacheReadFork.outA <> cacheWaitReadQ.enq
+  cache.read.req <> cacheReadFork.outB
+
+  val cacheReadJoin = Module(new StreamJoin(opWidthIdType, opType, opsAndId, joinOpIdOp)).io
+  cacheReadJoin.inA <> cacheWaitReadQ.deq
+  cacheReadJoin.inB <> cache.read.rsp
+
+  val addFork = Module(new StreamFork(opsAndId, syncOpType, idType, forkOps, forkId)).io
+  val adder = Module(p.makeAdd())
+  val adderIdQ = Module(new Queue(idType, adder.latency)).io
+  val addJoin = Module(new StreamJoin(opType, idType, opWidthIdType, joinOpId)).io
+
+  cacheReadJoin.out <> addFork.in
+  addFork.outA <> adder.io.in
+  addFork.outB <> adderIdQ.enq
+  addJoin.inA <> adder.io.out
+  addJoin.inB <> adderIdQ.deq
+
+  cache.write.req <> addJoin.out
+
+  // pop from UniqueQueue with writeComplete and count ops
+  shadow.deq.ready := cache.writeComplete
+
+  // register for counting completed operations
+  val regOpCount = Reg(init = UInt(0, 32))
+
+  val completedOp = shadow.deq.ready & shadow.deq.valid
+
+  when (!io.startRegular) {regOpCount := UInt(0)}
+  .otherwise {when (completedOp) {regOpCount := regOpCount + UInt(1)}}
+
+  // use op count to drive the doneRegular signal
+  io.doneRegular := (regOpCount === io.numNZ)
+
+  io.hazardStalls := Counter32Bit(shadow.hazard)
+}
