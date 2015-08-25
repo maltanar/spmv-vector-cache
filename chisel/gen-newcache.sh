e0e50ac74861a1b793debeee104a5103f7fd5d0f
Yaman Umuroglu <maltanar@gmail.com>
2015-08-25 17:34:07 +0200

    update gen scripts

--- chisel/gen-newcache.sh
+++ chisel/gen-newcache.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 ocmdepth="1024 2048 4096 8192 16384 32768"
+mlp="1 2 4 8 16"
 
 for d in $ocmdepth; do
-  echo "Spawning ocmDepth $d"
-  sbt "run inst NewCache --ocmDepth $d"
+  for m in $mlp; do
+    echo "Spawning ocmDepth $d mlp $m"
+    sbt "run inst NewCache --enableCMS --enableNB --ocmDepth $d --maxMiss $m --issueWindow $m"
+  done
 done
 
 wait
 echo "Verilog generation complete"
-cd ..
+
--- make-NewCache.sh
+++ make-NewCache.sh
@@ -2,11 +2,13 @@
 
 vpath="/home/maltanar/sandbox/spmv-vector-cache/chisel/verilogOutput"
 ocmdepth="1024 2048 4096 8192 16384 32768"
-#ocmdepth="1024 2048"
+mlp="1 2 4 8 16"
 
 for d in $ocmdepth; do
-  prj="NewCache-$d-6"
-  ./createSynth.sh "$prj" "$vpath/$prj/AXIAccelWrapper.v" > "logOutput/ProjGen-$prj.log"
+  for m in $mlp; do
+    prj="NewCache-cms-nb$m-$d-$m"
+    ./createSynth.sh "$prj" "$vpath/$prj/AXIAccelWrapper.v" > "logOutput/ProjGen-$prj.log"
+  done
 done
 
 echo "Projects created, running synthesis..."
@@ -15,15 +17,17 @@ cnt=0
 limit=2
 
 for d in $ocmdepth; do
-  prj="NewCache-$d-6"
-  echo "Launching $prj synthesis"
-  ./runSynth.sh $prj > "logOutput/ProjSynth-$prj-log" &
-  cnt=$((cnt+1))
-  if [ "$cnt" -ge "$limit" ]; then
-    echo "Job limit reached, waiting for completion..."
-    wait
-    cnt=$((cnt-1))
-  fi
+  for m in $mlp; do
+    prj="NewCache-cms-nb$m-$d-$m"
+    echo "Launching $prj synthesis"
+    ./runSynth.sh $prj > "logOutput/ProjSynth-$prj-log" &
+    cnt=$((cnt+1))
+    if [ "$cnt" -ge "$limit" ]; then
+      echo "Job limit reached, waiting for completion..."
+      wait
+      cnt=$((cnt-1))
+    fi
+  done
 done
 
 wait
